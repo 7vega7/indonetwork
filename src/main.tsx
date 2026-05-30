cc220ff21cfd04a5224059f34da43fb1f3cbeb04
7vega7 <jsgaudrey@gmail.com>
2026-05-30 22:38:47 +0700

    fix: perbaiki routing BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <App />
       <Toaster position="top-right" toastOptions={{
         style: { background: '#111130', color: '#e8eaf6', border: '1px solid rgba(0,200,255,0.2)' },
