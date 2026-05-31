eaf5b976502e15b9c659ee8e7543882aa7597d5f
7vega7 <jsgaudrey@gmail.com>
2026-06-01 04:14:14 +0700

    fix: ganti HashRouter ke BrowserRouter dengan _redirects Cloudflare Pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import App from './App'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
       <Toaster position="top-right" toastOptions={{
         style: { background: '#111130', color: '#e8eaf6', border: '1px solid rgba(0,200,255,0.2)' },
       }} />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 )
